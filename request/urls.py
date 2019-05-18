db92cee3d6e7b3b714866b470c1de80b3251bd6c
ToteBrick <402981948@qq.com>
2019-05-18 14:05:51 +0800

    路径拼接，参数按名称传递，参数可无序。

--- request/urls.py
+++ request/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('login_connect/([a-z]+)/(\d+)', views.login_connect, name='login')
+    url('login_connect/([a-z]+)/(\d+)', views.login_connect, name='login'), # 正则匹配,
+   # 命名参数按名字传递
+    url('login_connect2/(?P<city>[a-z]+)/(?P<code>\d+)', views.login_connect_sort, name='login')
+
 ]
--- request/views.py
+++ request/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def login_connect(request,city,code):
 
+# Create your views here.
+# 1. 提取正则url特定参数
+def login_connect(request, city, code):
     # param = request.GET
-    print('city:',city)
-    print('code:',code)
+    print('city:', city)
+    print('code:', code)
     return HttpResponse('URL路径拼接')
+
+
+# 参数无序，按正则名称匹配
+def login_connect_sort(request, code, city):
+    # param = request.GET
+    print('city:', city)
+    print('code:', code)
+    return HttpResponse('URL路径拼接按名称匹配')
