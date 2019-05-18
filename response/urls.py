c356f4c966817e9744be8c48b2080db450ab2856
ToteBrick <402981948@qq.com>
2019-05-18 20:07:38 +0800

    设置和读取指定cookie.

--- response/urls.py
+++ response/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('res/', views.res)
+    url('res/', views.res) ,
+    url('cookie/', views.cook_test) ,
 ]
--- response/views.py
+++ response/views.py
@@ -6,3 +6,11 @@ def res(request):
     response = HttpResponse(content='response body',status=200,content_type='text/html')
     # json格式的格式为content_type='application/json'
     return response
+
+# 设置Cookie与读取Cookie
+def cook_test(request):
+
+    response = HttpResponse('cookie')
+    response.set_cookie('name','Alice') # 设置cookie
+    print(request.COOKIES.get('name'))  # 读取指定Cookie
+    return response
