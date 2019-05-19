e53348bd1e296bb36b17ad065cc6a050a2f09d44
ToteBrick <402981948@qq.com>
2019-05-19 13:22:51 +0800

    '给类视图函数添加装饰类'

--- classsview/views.py
+++ classsview/views.py
@@ -5,8 +5,20 @@ from django.shortcuts import render
 from django.views.generic.base import View
 
 
+def decorator(fn):
+    def wrapper(self, request, *args, **kwargs):
+        print(request.method, request.path)
+        return fn(self, request, *args, **kwargs)
+
+    return wrapper
+
+
 class LoginView(View):
 
-    def get(self,request):
-
+    @decorator
+    def get(self, request):
         return HttpResponse('get 请求')
+
+    @decorator
+    def post(self, request):
+        return HttpResponse('post 请求')
