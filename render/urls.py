0c9b591d36a6f64567d05a8f30abc3d2d5f599c2
ToteBrick <402981948@qq.com>
2019-05-19 19:21:55 +0800

    filter的使用

--- render/urls.py
+++ render/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('index/', views.index)
+    url('index/', views.index),
+    url('filter/', views.filter),
 ]
--- render/views.py
+++ render/views.py
@@ -11,7 +11,7 @@ def index(request):
         },
         # 'alist': ['篮球', '游泳', '跑步']
         'alist': [],
-        'age':6
+        'age': 6
     }
 
     return render(request, 'index.html', context)
@@ -34,3 +34,17 @@ not
 注意：运算符左右两侧不能紧挨变量或常量，必须有空格。
 
 '''
+
+
+def filter(request):
+    context = {
+        'number': 10,
+        'a': '',
+        'b':12,
+        'adict': {
+            'name': 'Alice',
+            'age': 18
+        },
+        'alist': ['篮球', '游泳', '跑步']
+    }
+    return render(request, 'filter的使用.html', context)
