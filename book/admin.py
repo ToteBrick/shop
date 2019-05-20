3f3a4b1301f075304f635bbc388f24a001f7a576
ToteBrick <402981948@qq.com>
2019-05-20 15:53:12 +0800

    站点首页设置

--- book/admin.py
+++ book/admin.py
@@ -5,3 +5,8 @@ from book.models import BookInfo, HeroInfo
 
 admin.site.register(BookInfo)
 admin.site.register(HeroInfo)
+
+# 站点首页设置
+admin.site.site_header = '星空书城'
+admin.site.site_title = '星空书城MIS'
+admin.site.index_title = '欢迎使用星空书城MIS'
--- book/apps.py
+++ book/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookConfig(AppConfig):
     name = 'book'
-    verbose_name = '图书管理'
+    verbose_name = '图书管理系统'
